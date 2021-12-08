b08d24f12a6822838e1f57bc31f423795994ec0a
EmiV500 <eeveemc500@gmail.com>
2021-12-08 01:59:35 -0800

    FizzBuzz assignment completed

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+Emily Vorng 
+CSCI151 - FizzBuzz
+Prints out numbers 1 through 100. If it is a multiple of 3,
+it will print "Fizz" instead of the number. If it is a 
+multiple of 5, it will print "Buzz." If is a multiple of both,
+it will print "FizzBuzz" to the console.
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // Loops through numbers 1 - 100
+    for(int num = 1; num <= 100; num++) {
+        // Checks if current number is divisible by 3 or 5
+        if (num % 3 == 0) {
+            std::cout << "Fizz";
+        }
+
+        if (num % 5 == 0) {
+            std::cout << "Buzz";
+        }
+
+        if (num % 3 != 0 && num % 5 != 0) {
+            std::cout << num;
+        }
+        std::cout << "\n";
+    }
+}
